0a0a6f63e73a7f707608237acbf08aecbe586b4d
KronCyrcul <ibloko1504@gmail.com>
2018-08-09 21:03:17 +0300

    First commit

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,8 +3,8 @@ import pytz
 import requests
 
 
-def load_attempts(url, number_of_pages):
-    pages = number_of_pages
+def load_attempts(url, total_pages):
+    pages = total_pages
     for page in range(1, pages+1):
         params = {"page": page}
         response = requests.get(url, params=params)
@@ -21,16 +21,16 @@ def get_midnighters(users_attempts):
         attemp_timezone = attemp["timezone"]
         attemp_timestamp = attemp["timestamp"]
         utc_date = datetime.datetime.utcfromtimestamp(attemp_timestamp)
-        au_timezone = pytz.timezone(attemp_timezone)
-        au_date = au_timezone.normalize(utc_date.astimezone(au_timezone))
-        if au_date.hour <= 7 and au_date.hour >= 0:
+        user_timezone = pytz.timezone(attemp_timezone)
+        users_date = user_timezone.normalize(utc_date.astimezone(user_timezone))
+        if users_date.hour <= 7 and users_date.hour >= 0:
             yield attemp["username"]
 
 if __name__ == "__main__":
     request_url = "https://devman.org/api/challenges/solution_attempts/"
     response = requests.get(request_url)
-    number_of_pages = response.json()["number_of_pages"]
-    users_attempts = load_attempts(request_url, number_of_pages)
+    total_pages = response.json()["number_of_pages"]
+    users_attempts = load_attempts(request_url, total_pages)
     midnighters = get_midnighters(users_attempts)
     for user in midnighters:
         print(user)
